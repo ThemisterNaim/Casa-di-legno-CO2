e548ccb37b4b9a3f957d0e45ab5e9713f55b1de2
ThemisterNaim <96649980+ThemisterNaim@users.noreply.github.com>
2023-01-22 12:52:49 +0100

    Update Casa-Legno.ino

--- Casa-Legno.ino
+++ Casa-Legno.ino
@@ -10,25 +10,23 @@
   |    | /      \ | \  |___/  /      \ _______/ _______/ |
 */
 #include <Wire.h>            //Librerie
-#include <SparkFunCCS811.h>  //link per la libreria: https://github.com/sparkfun/SparkFun_CCS811_Arduino_Library/archive/refs/tags/v2.0.3.zip
+#include <SparkFunCCS811.h>  //Click here to get the library: http://librarymanager/All#SparkFun_CCS811
 #include <Servo.h>
-#define CCS811_ADDR 0x5B       //indirizzo I2C predefinito.
+#define CCS811_ADDR 0x5B       //Default I2C Address.
 CCS811 myCCS811(CCS811_ADDR);  // "nome " per evocare il comando.
 Servo myPorta;
 //Nomi dei pin
-#define servo 12 //Transistror che controlla servo.
-#define porta 10  //pin del servo.
-#define DC 9  //Transistor che controlla motore.
-#define camino 7        //Un semplice led giallo.
-#define errore 2         //Led che si attiva quando un cavo del sensore è sconesso.
-#define limite 1000 //Limite di particellle di CO2 per milione [ppm].
-int maniglia = 0; //Posizione del servo.
-bool link=0;
+bool NPN;            //Transistror digitale.
+#define servo 9      //pin del servo.
+#define camino 7     //Un semplice led giallo.
+#define errore 2     //Led che si attiva quando un cavo del sensore è sconesso.
+#define limite 1000  //Limite di particellle di CO2 per milione [ppm].
+int maniglia = 0;    //Posizione del servo.
 void controllo() {
   Serial.begin(115200);
   pinMode(errore, OUTPUT);
   Wire.begin();
-  if (myCCS811.begin() == false) {          //Se sensore non parte...
+  if (myCCS811.begin() == false) {  //Se sensore non parte...
     Serial.print("Controllare cavi");
     while (myCCS811.begin() == false) digitalWrite(errore, HIGH);  //...accendere Led mentre sensore non parte.
     Serial.println();
@@ -37,59 +35,53 @@ void controllo() {
 }
 
 void attivazione() {
-
+  Serial.println("link per codice e per proiezione ortogonale:");
+  Serial.println("https://github.com/ThemisterNaim/Casa-di-legno-CO2");
+  Serial.println();
   pinMode(servo, OUTPUT);
-  pinMode(porta, OUTPUT);
-  pinMode(DC, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW); //Colore (verde) non gradito
+  digitalWrite(LED_BUILTIN, LOW);
   pinMode(camino, OUTPUT);
-  myPorta.attach(porta);
-  digitalWrite(servo, HIGH);  //Necessario per dare corrente al servo, in questo modo si posizionerà automaticamente rispetto alla posizione dell'ultimo utilizzo
-  myPorta.write(0);
+  myPorta.attach(servo);
+  NPN = 0;
+
+  myPorta.write(24);
   Serial.println("Sistema attivato");
   while (myCCS811.dataAvailable() == false) {};  //Serve per prevenire la registrazione del falso positivo dato all'avvio
 }
 
 void aprire() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(camino, HIGH);
-  digitalWrite(servo, HIGH);  //Attiva il transistor che collega il ground al servo
-  while (maniglia <= 131) {     //Gira il Servo di 131° in senso orario. ma la porta si apre di circa 70
-    myPorta.write(maniglia);
-    delay(15);
-    maniglia++;
+  if (NPN == 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(camino, HIGH);
+    digitalWrite(NPN, HIGH);   //Attiva il transistor che collega il ground al servo
+    while (maniglia <= 160) {  //Gira il Serv
+      myPorta.write(maniglia);
+      delay(15);
+      maniglia++;
+    }
+    NPN = 1;
+    delay(500);
+    Serial.println("Porta aperta");
   }
-
-  delay(500);
-  digitalWrite(servo, LOW);
-  digitalWrite(DC, HIGH);  //Attiva il transistor che collega il motore DC
-  Serial.println("Motore attivato");
 }
-
 void chiudere() {
-  digitalWrite(DC, LOW);  //per prevenire "interferenze" tra il motore DC e il servo
+  if (NPN == 1) {
+    while (maniglia >= 24) {  //Gira il servo di 90° in senso anti orario per chiudere la servo
+      myPorta.write(maniglia);
+      delay(15);
+      maniglia--;
+    }
 
-  digitalWrite(servo, HIGH);
-  while (maniglia >= 0) {  //Gira il servo di 90° in senso anti orario per chiudere la porta
-    myPorta.write(maniglia);
-    delay(15);
-    maniglia--;
+    Serial.println("Porta chiusa");
+    delay(10);
+    digitalWrite(camino, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    NPN = 0;
   }
-
-  digitalWrite(servo, LOW);
-  Serial.println("Motore disattivato");
-  delay(10);
-  digitalWrite(camino, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  
 }
 
 void setup() {
-  if (link ==0){
-Serial.println("https://github.com/ThemisterNaim/Casa-di-legno-CO2");
-link=1;
-  }
   controllo();
   attivazione();
 }
@@ -98,22 +90,19 @@ void loop() {
 
   if (myCCS811.dataAvailable()) {  //Se sono stati registrati i dati
 
-    myCCS811.readAlgorithmResults(); //legge i dati registrati
-    int co2 = myCCS811.getCO2();  //assegna ad una variabile il valore della CO2
+    myCCS811.readAlgorithmResults();  //legge i dati registrati
+    int co2 = myCCS811.getCO2();      //assegna ad una variabile il valore della CO2
     Serial.print("CO2: ");
     Serial.print(co2);
     Serial.print(" ppm");
     Serial.println();
 
     if (co2 >= limite) aprire();  //se il valore della CO2 è maggiore di 1000
-    
-    else {
-      if (digitalRead(DC) == HIGH) digitalWrite(DC, LOW);  //controllare se il motore è ancora attivato
-      chiudere();
-    }
-    
+
+    else chiudere();
+
     Serial.println();
-    delay(999);  //Non si deve usare costantemente il bus I2C
+    delay(1000);  //Non si deve usare costantemente il bus I2C
   } else {
     if (myCCS811.dataAvailable() == false) setup();
     Serial.println();
